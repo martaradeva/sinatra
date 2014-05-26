fc9241b0db06212da4e3ad4d9e9584403f149cac
Martina Radeva <martaradeva@yahoo.com>
2014-05-26 22:51:53 +0300

    use unix //

--- ruby/parse_erb.rb
+++ ruby/parse_erb.rb
@@ -3,30 +3,31 @@ require 'yaml'
 require 'erb'
 require 'sinatra'
 
+# template_path = File.dirname(__FILE__) + '/../html'
 
 get '/index.html' do
-	index_body = File.read('.\\html_templates\\index.html.erb')
-	ideas = YAML.load_file(".\\libraries\\ideas.yml")
+	index_body = File.read('./html_templates/index.html.erb')
+	ideas = YAML.load_file("./libraries/ideas.yml")
 	rendered_html = ERB.new(index_body).result(binding)
-	# File.write("..\\rubytest\\home.html", rendered_html)
+	# File.write("../rubytest/home.html", rendered_html)
 	rendered_html
 end
 
 get '/#{idea_number}.html' do
-	idea_body = File.read('.\\html_templates\\idea_view.html.erb')
-	ideas = YAML.load_file(".\\libraries\\ideas.yml")
+	idea_body = File.read('./html_templates/idea_view.html.erb')
+	ideas = YAML.load_file("./libraries/ideas.yml")
 	ideas[:idea_number]
 	rendered_html = ERB.new(idea_body).result(binding)
-	# File.write("..\\rubytest\\home.html", rendered_html)
+	# File.write("../rubytest/home.html", rendered_html)
 	rendered_html
 end
 
 get '/about.html' do
-	about_page = File.read('.\\html_templates\\about.html')
+	about_page = File.read('./html_templates/about.html')
 	about_page
 end
 
 get '/upload.html' do
-	upload_page = File.read('.\\html_templates\\upload.html')
+	upload_page = File.read('./html_templates/upload.html')
 	upload_page
 end
