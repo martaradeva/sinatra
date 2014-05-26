6fb4572f485e79fc9418a9759b783897e895ee39
Martina Radeva <martaradeva@yahoo.com>
2014-05-26 20:06:14 +0300

    static about page added to parse_erb.rb

--- ruby/parse_erb.rb
+++ ruby/parse_erb.rb
@@ -4,12 +4,17 @@ require 'erb'
 require 'sinatra'
 
 
-get '/home.html' do
-	website_body = File.read('..\\html_templates\\home.html.erb')
+get '/index.html' do
+	website_body = File.read('..\\html_templates\\index.html.erb')
 	ideas = YAML.load_file("..\\libraries\\ideas.yml")
 	rendered_html = ERB.new(website_body).result(binding)
 	# File.write("..\\rubytest\\home.html", rendered_html)
 	rendered_html
+end
+
+get '/about.html' do
+	about_page = File.read('..\\html_templates\\about.html')
+	about_page
 end
 
 # hello_world_app = MyAwesomeWebApp.new
